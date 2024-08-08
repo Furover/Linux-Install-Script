376f28836839f946f2e60ee5c3f4530794522b69
Furover <133924982+Furover@users.noreply.github.com>
2024-08-08 01:08:38 +0000

    Copies some icons I made into the icons folder

---
+++ send-those-icons.sh
@@ -0,0 +1,2 @@
+cp ~/Pictures/gtk-theme-icons/{eyedropper.svg,itch-app.svg,mousai.svg,notion.svg,portmaster.svg,zed.svg} ~/.local/share/icons/Colloid-Pink-Light/apps/scalable/
+ln -s ~/.local/share/icons/Colloid-Pink-Light/apps/scalable/discord.svg ~/.local/share/icons/Colloid-Pink-Light/apps/scalable/dev.vencord.Vesktop.svg
