b1cad6ae0305693a8707f8aadd00c2b67f429fec
Furover <133924982+Furover@users.noreply.github.com>
2024-08-08 01:43:09 +0000

    Forgot to change this one on here

--- send-those-icons.sh
+++ send-those-icons.sh
@@ -1,2 +1,2 @@
-cp ~/Pictures/gtk-theme-icons/{eyedropper.svg,itch-app.svg,mousai.svg,notion.svg,portmaster.svg,zed.svg} ~/.local/share/icons/Colloid-Pink-Light/apps/scalable/
+cp Icons/{eyedropper.svg,itch-app.svg,mousai.svg,notion.svg,portmaster.svg,zed.svg} ~/.local/share/icons/Colloid-Pink-Light/apps/scalable/
 ln -s ~/.local/share/icons/Colloid-Pink-Light/apps/scalable/discord.svg ~/.local/share/icons/Colloid-Pink-Light/apps/scalable/dev.vencord.Vesktop.svg
